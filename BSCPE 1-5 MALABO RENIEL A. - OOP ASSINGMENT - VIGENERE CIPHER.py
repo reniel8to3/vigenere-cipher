376a37dfab56424c33d9494ff64c15ab7039ba6d
reniel8to3 <renielalmariomalabo@gmail.com>
2023-04-21 19:09:16 +0800

    define main function + ask user for input

--- BSCPE 1-5 MALABO RENIEL A. - OOP ASSINGMENT - VIGENERE CIPHER.py
+++ BSCPE 1-5 MALABO RENIEL A. - OOP ASSINGMENT - VIGENERE CIPHER.py
@@ -1,7 +1,17 @@
 #create dictionary
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define main function
+def main():
 #ask user for input
+print ('Hello!  This program is designed to create a Vegenere Cipher.')
+    plaintext = input("What do you want to decipher? Please use uppercase letters with no spaces only. ")
+    keyword = input("What is the key to the code? Please use uppercase letters only. ")
+    secret_code = encrypt(plaintext, keyword)
+    secret_revealed = decrypt(secret_code, keyword)
+
+    print('Deciphering...')
+    print("The resulting Vigenere Cipher is: " + secret_code)
+    print("Your code is : " + secret_revealed)
 #create defining functions
 #create encrypt function
 #create decrypt function
